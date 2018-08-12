e8eaa0dac93c6321e48eb2a07f7df9024d1938a6
jnavb <jorgenavb@gmail.com>
2018-08-12 23:44:58 +0200

    Add data to detailPark page

--- src/pages/park-details/park-details.ts
+++ src/pages/park-details/park-details.ts
@@ -14,12 +14,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'park-details.html',
 })
 export class ParkDetailsPage {
+	parkInfo: Object;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  	this.parkInfo = navParams.data.parkData;
+  	console.log(this.parkInfo);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ParkDetailsPage');
   }
-
 }
